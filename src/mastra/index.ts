fc1f538ca496be278240d9279a764fad6e76d6cf
DESKTOP-9N45CRK\Yangc <15315575962@163.com>
2025-05-18 00:55:46 +0800

    init

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,39 +1,45 @@
 import { Mastra } from '@mastra/core/mastra';
 import { createLogger } from '@mastra/core/logger';
 import { LibSQLStore } from '@mastra/libsql';
-import { CloudflareDeployer } from "@mastra/deployer-cloudflare";
+import { CloudflareDeployer } from '@mastra/deployer-cloudflare';
 
 import { codeReviewAgent } from './agents';
 
 export const mastra = new Mastra({
   deployer: new CloudflareDeployer({
-    scope: "d5b3bb970da79f07a9fb614e60d02766",
-    projectName: "yc-mastra-app",
+    scope: 'd5b3bb970da79f07a9fb614e60d02766',
+    projectName: 'yc-mastra-app',
     routes: [
       {
-        pattern: "deepseek-pages-demo.pages.dev/*",
-        zone_name: "deepseek-pages-demo.pages.dev",
+        pattern: 'yc-mastra-app.yangcongzhao123.workers.dev/*',
+        zone_name: 'yangcongzhao123.workers.dev',
         custom_domain: false,
       },
       {
-        pattern: "zhaoyangkuajing.cyou/*",
-        zone_name: "zhaoyangkuajing.cyou",
+        pattern: 'zhaoyangkuajing.cyou/*',
+        zone_name: 'zhaoyangkuajing.cyou',
         custom_domain: true,
       },
     ],
-    workerNamespace: "3223a366bb61449b8eaa2981622663d9",
+    workerNamespace: '3223a366bb61449b8eaa2981622663d9',
     auth: {
-      apiToken: "UM7POzRkIjAPY3h7EsQ6v16x3fwRE1x04UYJ2og0",
-      apiEmail: "yangcongzhao123@gmail.com",
+      apiToken: process.env.CLOUDFLARE_API_TOKEN,
+      apiEmail: 'yangcongzhao123@gmail.com',
     },
+
   }),
   agents: { codeReviewAgent },
   storage: new LibSQLStore({
-    // stores telemetry, evals, ... into memory storage, if it needs to persist, change to file:../mastra.db
-    url: ":memory:",
+    url: ':memory:',
   }),
   logger: createLogger({
     name: 'Mastra',
     level: 'info',
   }),
+  server: {
+    build: {
+      openAPIDocs: true,
+      swaggerUI: true,
+    },
+  },
 });
