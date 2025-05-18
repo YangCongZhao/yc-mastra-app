546a275fd7c34bbc4721aca9099593f0cb9478f8
DESKTOP-9N45CRK\Yangc <15315575962@163.com>
2025-05-18 12:54:10 +0800

    init

--- src/mastra/index.ts
+++ src/mastra/index.ts
@@ -1,22 +1,22 @@
 import { Mastra } from '@mastra/core/mastra';
 import { createLogger } from '@mastra/core/logger';
 import { LibSQLStore } from '@mastra/libsql';
-import { CloudflareDeployer } from "@mastra/deployer-cloudflare";
+// import { CloudflareDeployer } from "@mastra/deployer-cloudflare";
 
 import { codeReviewAgent } from './agents';
 
 export const mastra = new Mastra({
-  deployer: new CloudflareDeployer({
-    scope: "d5b3bb970da79f07a9fb614e60d02766",
-    projectName: "yc-mastra-app",
-    routes: [
-    ],
-    workerNamespace: "3223a366bb61449b8eaa2981622663d9",
-    auth: {
-      apiToken: "UM7POzRkIjAPY3h7EsQ6v16x3fwRE1x04UYJ2og0",
-      apiEmail: "yangcongzhao123@gmail.com",
-    },
-  }),
+  // deployer: new CloudflareDeployer({
+  //   scope: "d5b3bb970da79f07a9fb614e60d02766",
+  //   projectName: "yc-mastra-app",
+  //   routes: [
+  //   ],
+  //   workerNamespace: "3223a366bb61449b8eaa2981622663d9",
+  //   auth: {
+  //     apiToken: "UM7POzRkIjAPY3h7EsQ6v16x3fwRE1x04UYJ2og0",
+  //     apiEmail: "yangcongzhao123@gmail.com",
+  //   },
+  // }),
   server: {
     // 配置CORS，解决跨域问题
     cors: {
